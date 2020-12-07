14b74408627fddb5276118807fdbfea7189ddb4e
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-07 21:03:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-# don't forget to add: require 'pry'
+
 
 def generate_star_date
     (rand(100000) + 400000) / 10.0
@@ -12,3 +12,5 @@ def engage
   puts state_log(date)
   return state_log
 end
+
+engage
